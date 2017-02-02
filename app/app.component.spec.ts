91f6efff1e2752e7a5935ec1a395161fb8df770e
DIMITRIOS KYRITSIS <dimitrioskyritsis@DIMITRIOSs-MacBook-Pro.local>
2017-02-02 08:36:43 +0100

    added app component async integration test

---
+++ app/app.component.spec.ts
@@ -0,0 +1,43 @@
+/* tslint:disable:no-unused-variable */
+
+import {TestBed, async} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+import {RouterTestingModule} from "@angular/router/testing";
+
+
+describe('App: Testing', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+      imports: [RouterTestingModule]
+    });
+
+  });
+
+  it('should create the app', async(() => {
+    TestBed.compileComponents().then(() => {
+      let fixture = TestBed.createComponent(AppComponent);
+      let app = fixture.debugElement.componentInstance;
+      expect(app).toBeTruthy();
+    })
+  }));
+
+  it(`should have as title 'Tour of heroes'`, async(() => {
+    TestBed.compileComponents().then(() => {
+      let fixture = TestBed.createComponent(AppComponent);
+      let app = fixture.debugElement.componentInstance;
+      expect(app.title).toEqual('Tour of Heroes');
+    })
+  }));
+
+  it('should render title in a h1 tag', async(() => {
+    TestBed.compileComponents().then(() => {
+      let fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      let compiled = fixture.debugElement.nativeElement;
+      expect(compiled.querySelector('h1').textContent).toContain('Tour of Heroes');
+    })
+  }));
+});
--- app/app.component.ts
+++ app/app.component.ts
@@ -14,5 +14,5 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  title = 'Tour of Heroes';
+  public title = 'Tour of Heroes';
 }
